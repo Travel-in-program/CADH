3fe78d664a36cc50ac2ab72c91fbe432c09d9f23
Travel-in-program <95960234+Travel-in-program@users.noreply.github.com>
2025-10-07 17:52:54 +0800

    Update main1.py

--- CADH/main1.py
+++ CADH/main1.py
@@ -7,10 +7,10 @@ from torch.utils.data import DataLoader, Dataset, random_split
 import random
 from load_data import get_source_data, get_target_data
 from model import ProjectionResidualFusion, hash_encoder, Classifier,share_DomainClassifier,AllModel
-from loss import *  # 你可以根据自己定义的损失函数替换
+from loss import *  
 from tqdm import tqdm
 from torchvision import transforms
-from feature_extra import CustomImageDataset,TransformerEncoder
+#from feature_extra import CustomImageDataset,TransformerEncoder
 from sklearn.model_selection import train_test_split
 import scipy.io as sio
 #from test import performance_eval_real#performance_eval,evaluate,build_codes_and_labels,mean_average_precision1,
@@ -103,45 +103,42 @@ def compress1(database_loader, query_loader, model):
 
     return re_BI, re_L, qu_BI, qu_L
 
-#伪标签生成策略：先根据共享特征提取器生成的特征图提取中心，对标到伪标签上
+
 def compute_centers(x, psedo_labels, num_cluster):
     n_samples = x.size(0)
     #print(n_samples)
     if len(psedo_labels.size()) > 1:
-        # 如果伪标签 psedo_labels 是一个二维张量，则将其转置得到一个形状为 (n_samples, num_cluster) 的张量
+        
         weight = psedo_labels.T
         #print(weight)
     else:
-        # 如果伪标签 psedo_labels 是一个一维张量，则创建一个形状为 (num_cluster, n_samples) 的零张量 weight，
-        # 并将其中第 psedo_labels[i] 行、第 i 列的元素设置为 1，表示第 i 个样本属于第 psedo_labels[i] 个聚类
+        
         weight = torch.zeros(num_cluster, n_samples).to(x)  # L, N
         weight[psedo_labels, torch.arange(n_samples)] = 1
 
     weight = weight.float()
-    # 对 weight 进行 L1 归一化，即将每一行的元素值都除以该行的元素之和，以确保每个聚类的权重之和为 1
+    
     weight = F.normalize(weight, p=1, dim=1)  # l1 normalization
-    # 通过矩阵乘法 torch.mm(weight, x) 将 weight 和 x 相乘，得到每个聚类的样本特征的加权平均值，即聚类中心
+    
     centers = torch.mm(weight, x)
-    # 对聚类中心进行 L2 归一化，以确保每个聚类中心向量的长度为1
+    
     centers = F.normalize(centers, dim=1)
     return centers
 @torch.no_grad()
 def psedo_labeling(num_cluster, batch_features, centers, batch_target):
     l2_normalize =True
     torch.cuda.empty_cache()
-    # (1) 进行L2归一化
+   
     if l2_normalize:
         batch_features = F.normalize(batch_features, dim=1)
         batch_features_cpu = batch_features.cpu()
-    # (2) 计算batch_feat和source_centers的相似度矩阵
-    # 为了使用F.cosine_similarity函数,需要将cluster_centers和centers分别扩展成大小为(c, 1, d)和(1, n, d)的三维张量,以便在第二维上进行广播操作
-    # todo ok
+    
     centers_cpu = centers.cpu()
     btarget_cen_similarity = F.cosine_similarity(batch_features_cpu.unsqueeze(1), centers_cpu.unsqueeze(0), dim=2)
 
-    # (3) 得到源域和目标域标签的对应关系
+    
     relation = torch.zeros(num_cluster, dtype=torch.int64) - 1
-    # 对similarity中的每一行进行排序,并按照从大到小的顺序记录索引
+    
     sorted_indices = torch.argsort(btarget_cen_similarity, dim=1, descending=True)
     new_cluster_labels = sorted_indices[:, 0]
 
@@ -180,19 +177,14 @@ def train(args,model, source_loader, target_loader,test_loader,num_classes, devi
             '''p = float(i + epoch * steps) / epochs / steps
             alpha = 2. / (1. + np.exp(-10 * p)) - 1'''
             all = model(source_data,target_data,lambda_=1)
-            # shared_source_data,shared_target_data,\
-            # hash_source_data,hash_target_data,\
-            # = all['shared_feat_source'],all['shared_feat_target'],\
-            #                  all['hash_code_source'],all['hash_code_target'],\
-                             
+                           
             shared_source_data,shared_target_data,domain_source_data,domain_target_data,\
             proj_source_data,proj_target_data,hash_source_data,hash_target_data,\
             class_logits_source,class_logits_target,dom_src,dom_tgt,dom_spe_src,dom_spe_tag= all['shared_feat_source'],all['shared_feat_target'],all['domain_feat_source'],all['domain_feat_target'],\
                            all['fused_feat_source'],all['fused_feat_target'],all['hash_code_source'],all['hash_code_target'],\
                            all['class_logits_source'],all['class_logits_target'],all['dom_src'],all['dom_tgt'],all['dom_spe_src'],all['dom_spe_tag']
             
-            #生成伪标签c
-            #计算源域的原型
+            
             
             batch_source_centers = compute_centers(shared_source_data, source_labels.squeeze(), num_classes)
             memory_source_centers = memory_source_centers.detach() + batch_source_centers
@@ -213,12 +205,12 @@ def train(args,model, source_loader, target_loader,test_loader,num_classes, devi
                 target_plabel = psedo_labeling(num_classes, shared_target_data, memory_target_centers,target_label)
 
             # 计算损失
-            #域损失domain_loss为交叉熵损失     ys与yt均为one-hot编码的标签
+         
             # 域分类损失
             pred_tgt = class_logits_target
             #目标域的伪标签
             pseudo_labels1 = target_plabel.to(device)
-            #print(pseudo_labels1)
+         
             #计算的个数
             correct_sample = torch.sum(pred_tgt.argmax(dim=1) == target_label.squeeze())
             correct += correct_sample
@@ -230,15 +222,11 @@ def train(args,model, source_loader, target_loader,test_loader,num_classes, devi
 
             #域对齐损失
 
-            #loss_domain = domain_adversarial_loss(dom_src, dom_tgt)
+            loss_domain = domain_adversarial_loss(dom_src, dom_tgt)
 
         #掩码损失#设置掩码块，并初始化一个mask的实例
-            '''mask_ratio =args.mask_ratio
-            if epoch>=40:
-                mask_ratio = 0.3
-            mask_generator = FeatureMasker(mask_ratio=mask_ratio, block_size=args.block_size)#111111111111111
-            loss_mask = mask_consistency_loss(model, class_logits_target,target_data, mask_generator,pseudo_labels1, num_classes)'''
-            mask_generator = FeatureMasker(mask_ratio=args.mask_ratio, block_size=args.block_size)#111111111111111
+            
+            mask_generator = FeatureMasker(mask_ratio=args.mask_ratio, block_size=args.block_size)#
             loss_mask = mask_consistency_loss(model, class_logits_target,target_data, mask_generator, pseudo_labels1,num_classes)
         #分类损失
             loss_cls = classification_loss(pred_src, source_labels,pred_tgt, pseudo_labels1)
@@ -246,25 +234,22 @@ def train(args,model, source_loader, target_loader,test_loader,num_classes, devi
             # 计算哈希损失
             #编码器损失
          #mmd损失
-            #mmd_loss = MMDLoss(kernel_type='rbf', kernel_mul=args.kernel_mul, kernel_num=args.kernel_num)#11111111111111    1.5    7
+            #mmd_loss = MMDLoss(kernel_type='rbf', kernel_mul=args.kernel_mul, kernel_num=args.kernel_num)#
             #loss_coral = mmd_loss(shared_source_data,shared_target_data)#mmd_loss+(domain_source_data,domain_target_data)
             #源域的one—hot编码标签
             source_labels_onehot = F.one_hot(source_labels, num_classes=num_classes).float()
-        # 计算哈希对齐损失
-        #参考崔老师的程序，对照一边
-            #print(pseudo_labels1.shape,source_labels_onehot.shape)
+        
+            
             sim_matrix = torch.mm(source_labels_onehot,F.one_hot(pseudo_labels1,num_classes=num_classes).float().t())  # 计算相似度矩阵
-            #print(hash_source_data)#不是哈希码
-
-            #print(hash_source_code)
+            
             #量化损失可以多加入一个目标与的损失
-            loss_hash = hash_pairwise_loss(hash_source_data, hash_target_data, sim_matrix, alpha=args.alpha)#  0.0008               11111111111111
+            loss_hash = hash_pairwise_loss(hash_source_data, hash_target_data, sim_matrix, alpha=args.alpha)#  0.0008               
             #loss_quant = quantization_loss(hash_source_data)
 
             total_loss = (
                 loss_weights["cls"] * loss_cls +
                 loss_weights["dmc"] * loss_dmc +
-                #loss_weights["domain"] * loss_domain +
+                loss_weights["domain"] * loss_domain +
                 loss_weights["hash"] * loss_hash 
                 +loss_weights["mask"] * loss_mask
                 #+loss_weights["coral"] * loss_coral
@@ -285,7 +270,7 @@ def train(args,model, source_loader, target_loader,test_loader,num_classes, devi
             print({
             "loss_cls": f"{loss_cls.item():.4f}",
             "loss_dmc": f"{loss_dmc.item():.4f}",
-            #"loss_domain": f"{loss_domain.item():.4f}",
+            "loss_domain": f"{loss_domain.item():.4f}",
             "loss_hash": f"{loss_hash.item():.4f}",
             "loss_mask": f"{loss_mask.item():.4f}",
             #"loss_coral": f"{loss_coral.item():.4f}",
@@ -311,7 +296,7 @@ def train(args,model, source_loader, target_loader,test_loader,num_classes, devi
                 print('total acc: %.8f' %(acc))
         i+=1
     performance_eval_real(model, source_loader,test_loader)
-    save_path = '/media/abc/ware/xsh/uad/program5_tiv/file/mnist/ptor2.pth'
+    save_path = '/media/abc/ware/uad/program5_tiv/file/mnist/ptor2.pth'
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
     torch.save(model.state_dict(), save_path)
     print(f"Model saved to {save_path}")
@@ -338,23 +323,17 @@ if __name__ == "__main__":
 
 
 
-    # base_path  = '/media/abc/ware/xsh/uad/data/use_data_tiv/mnist/'#'/media/abc/ware/xsh/uad/data/use_data_tiv/office_home/''/media/abc/ware/xsh/uad/data/use_data/'
+    # base_path  = '/media/abc/ware/uad/data/use_data_tiv/mnist/'#'/media/abc/ware/uad/data/use_data_tiv/office_home/''/media/abc/ware/uad/data/use_data/'
     # source_domain = 'usps_mat_vit_b16'#'Real_World_feature_mat   usps_mat_vit_b16'
     # print(source_domain)
     # target_domain = 'Product_feature_mat'#'mnist_mat_vit_b16'
-    '''source_loader, n_class, dim1 = get_source_data(base_path, source_domain,batchsize)
-    print(n_class,dim1)
-    target_loader = get_target_data(base_path, target_domain,batchsize)
-    #训练集的数据量3995
-    target_train_loader = target_loader['train']
-    target_test_loader = target_loader['query']'''
 
 
 
     source_domain = args.domain.split('To')[0]#
     target_domain = args.domain.split('To')[1]#
     if args.dataset == 'Office-Home':  # (1) Art (2) Clipart (3) Product (4) Real_World
-        base_path = '/media/abc/ware/xsh/uad/data/use_data_tiv/office_home/'#'/media/abc/ware/xsh/uad/data/use_data_tiv/office_home/'
+        base_path = '/media/abc/ware/uad/data/use_data_tiv/office_home/'#'/media/abc/ware/uad/data/use_data_tiv/office_home/'
         source_loader, n_class, dim1 = get_source_data(base_path, source_domain,args.batchsize)
         target_loader = get_target_data(base_path, target_domain,args.batchsize)
         print(len(target_loader))
@@ -362,14 +341,14 @@ if __name__ == "__main__":
         target_test_loader = target_loader['query']
 
     elif args.dataset == 'Office-31':  # (1) Amazon (2) Dslr (3) Webcam
-        base_path = '/media/abc/ware/xsh/uad/data/use_data_tiv/office31/'
+        base_path = '/media/abc/ware/uad/data/use_data_tiv/office31/'
         source_loader, n_class, dim1 = get_source_data(base_path, source_domain,args.batchsize)
         target_loader = get_target_data(base_path, target_domain,args.batchsize)
         target_train_loader = target_loader['train']
         target_test_loader = target_loader['query']
 
     elif args.dataset == 'Digits':
-        base_path = '/media/abc/ware/xsh/uad/data/use_data_tiv/mnist/'
+        base_path = '/media/abc/ware/uad/data/use_data_tiv/mnist/'
         source_loader, n_class, dim1 = get_source_data(base_path, source_domain,args.batchsize)
         target_loader = get_target_data(base_path, target_domain,args.batchsize)
         target_train_loader = target_loader['train']
